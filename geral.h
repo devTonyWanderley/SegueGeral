06776e7144e01c966f0a7d1e6ab37b4d3f4f702d
devTonyWanderley <devtonywanderley@gmail.com>
2025-12-07 14:20:30 -0300

    Feita a apresentação em geral.h

--- geral.h
+++ geral.h
@@ -1,4 +1,13 @@
 /** @date   07/12/25    07:58
+ *  @brief  Capacidade pra decodificar arquivos texto,
+ *          a princípio, de comprimento fixo.
+ *          A segunda etapa será mesclar com identificação
+ *          de bandeiras no corpo do texto que separam dados,
+ *          e posteriormente, duas estruturas de dados
+ *          diferentes, chegando aí à leitura de arquivo do
+ *          tipo caderneta gd5 onde temos estações e visadas.
+ *          Ópta por fazê-lo parametrizado, de forma que se
+ *          possa usar o presente pra outras situações similares.
  */
 #ifndef GERAL_H
 #define GERAL_H
